f1f19c56989e696796f3685392d1140ceee55fa9
Suguru.Ueda <stonebeach.dakar@gmail.com>
2021-09-11 03:18:34 +0000

    place App追加、モデル作成

---
+++ place/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from django.db.models.fields.related import ForeignKey
+
+User = get_user_model()
+
+# Create your models here.
+class Place(models.Model):
+    owner = ForeignKey(User, verbose_name="owenr", null=False, on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="name", null=False, max_length=100)
+    address = models.CharField(verbose_name="address", null=False, max_length=200)
+    url = models.URLField(verbose_name="homepage_url", null=True)
+    is_active = models.BooleanField(verbose_name="active", null=False, default=True)
+
+    class Meta:
+        db_table = "place"
