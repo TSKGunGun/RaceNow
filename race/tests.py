2bc358023c9022022829b3955c8670a78e52d36d
Suguru.Ueda <stonebeach.dakar@gmail.com>
2021-09-14 06:59:51 +0000

    racestatusのデフォルト値作成、テスト作成

--- race/tests.py
+++ race/tests.py
@@ -3,7 +3,7 @@ from .models import Race, RaceStatus, Category, RaceType
 
 # Create your tests here.
 class Custom_TestCase(TestCase):
-   fixtures = ['race_default.json']
+   fixtures = ['race_default.json', 'race_status_default']
 
 class CategoryTest(Custom_TestCase):
     def test_defaultItems(self):
@@ -18,3 +18,10 @@ class RaceTypeTest(Custom_TestCase):
         self.assertEqual(racetype.get(pk=1).name, "サーキット")
         self.assertEqual(racetype.get(pk=2).name, "モトクロス")
         self.assertEqual(racetype.get(pk=3).name, "クロスカントリー")
+
+class RaceStatusTest(Custom_TestCase):
+    def test_defaultItems(self):
+        self.assertEqual(RaceStatus.objects.get(pk=1).name, "レース開催前")
+        self.assertEqual(RaceStatus.objects.get(pk=2).name, "レース開催中")
+        self.assertEqual(RaceStatus.objects.get(pk=3).name, "レース終了")
+        self.assertEqual(RaceStatus.objects.get(pk=4).name, "レース中止")
