be12268cd0d8e13eca8e81af162e0f97d7d0dc49
Suguru.Ueda <stonebeach.dakar@gmail.com>
2021-09-14 06:46:48 +0000

    category,racetypeのデフォルト値作成

--- race/tests.py
+++ race/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import Race, RaceStatus, Category, RaceType
 
 # Create your tests here.
+class Custom_TestCase(TestCase):
+   fixtures = ['race_default.json']
+
+class CategoryTest(Custom_TestCase):
+    def test_defaultItems(self):
+        
+        self.assertEqual(Category.objects.get(pk=1).name, "オンロードレース")
+        self.assertEqual(Category.objects.get(pk=2).name, "オフロードレース")
+
+class RaceTypeTest(Custom_TestCase):
+    def test_defalutItems(self):
+        racetype = RaceType.objects.all()
+
+        self.assertEqual(racetype.get(pk=1).name, "サーキット")
+        self.assertEqual(racetype.get(pk=2).name, "モトクロス")
+        self.assertEqual(racetype.get(pk=3).name, "クロスカントリー")
