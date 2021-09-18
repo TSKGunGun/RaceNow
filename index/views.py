5951ab41befc11cf4ba9c892f1b3710dbeddeaa8
Suguru.Ueda <stonebeach.dakar@gmail.com>
2021-09-18 01:53:25 +0000

    新着レースをインデクスページに追加

--- index/views.py
+++ index/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from race.models import Race
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = "index/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["new_arrivals"] = Race.objects.order_by('created_at').reverse()[:3]
+
+        return context
