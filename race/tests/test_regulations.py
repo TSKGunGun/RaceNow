de1faac6bafbd927bd79943cdab27bee1c0fbf2b
Suguru.Ueda <stonebeach.dakar@gmail.com>
2021-10-05 01:21:57 +0000

    レギュレーションのテストケースをエラーが起きないよう修正

--- race/tests/test_regulations.py
+++ race/tests/test_regulations.py
@@ -64,43 +64,80 @@ class Race_Model_Test(TestCase):
         self.client.force_login(self.user)
 
         self.assertEqual(Race.objects.get(pk=self.race.id).status.id, RaceStatus.RACE_STATUS_DEFAULT )
-        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}))
 
-        self.assertEqual(response.status_code, 200)
+        params = {
+            "is_teamrace" : False,
+            "teammember_count_min" : 1,
+            "teammember_count_max" : 1,
+            "is_heat" : False,
+            "heat_count" : 1
+        }
 
-    def test_get_regulationsetup_notlogin(self):
+        self.assertFalse(Race.objects.get(pk=self.race.id).is_regulationsetuped)
+        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}), params)
+
+        self.assertEqual(response.status_code, 302)
+        self.assertTrue(Race.objects.get(pk=self.race.id).is_regulationsetuped)
+
+    def test_post_regulationsetup_notlogin(self):
         self.client.logout()
 
         self.assertEqual(Race.objects.get(pk=self.race.id).status.id, RaceStatus.RACE_STATUS_DEFAULT )
-        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}))
+
+        params = {
+            "is_teamrace" : False,
+            "teammember_count_min" : 1,
+            "teammember_count_max" : 1,
+            "is_heat" : False,
+            "heat_count" : 1
+        }
+        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}), params)
 
         self.assertNotEqual(response.status_code, 200)
 
-    def test_get_regulationsetup_notMember(self):
+    def test_post_regulationsetup_notMember(self):
         self.client.logout()
         self.client.force_login(UserFactory())
 
         self.assertEqual(Race.objects.get(pk=self.race.id).status.id, RaceStatus.RACE_STATUS_DEFAULT )
-        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}))
+
+        params = {
+            "is_teamrace" : False,
+            "teammember_count_min" : 1,
+            "teammember_count_max" : 1,
+            "is_heat" : False,
+            "heat_count" : 1
+        }
+        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}), params)
 
         self.assertNotEqual(response.status_code, 200)
 
-    def test_get_regulationsetup_notMatchStatus(self):
+    def test_post_regulationsetup_notMatchStatus(self):
         self.client.logout()
         self.client.force_login(self.user)
         
-        invalid_statuses = [RaceStatus.RACE_STATUS_ENTRY, RaceStatus.RACE_STATUS_HOLD, RaceStatus.RACE_STATUS_END, RaceStatus.RACE_STATUS_CANCEL]
+        invalid_statuses = [RaceStatus.RACE_STATUS_ENTRY, RaceStatus.RACE_STATUS_HOLD, RaceStatus.RACE_STATUS_END, RaceStatus.RACE_STATUS_CANCEL]        
+        
+        params = {
+            "is_teamrace" : False,
+            "teammember_count_min" : 1,
+            "teammember_count_max" : 1,
+            "is_heat" : False,
+            "heat_count" : 1
+        }
         
         for status in invalid_statuses:
             with self.subTest(status=status):
                 self.race.status = RaceStatus.objects.get(id=status)
                 self.race.save()
                 self.assertEqual(Race.objects.get(pk=self.race.id).status.id, status )
-                response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}))
+
+
+                response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}), params)
                 self.assertNotEqual(response.status_code, 200)
 
         self.race.status = RaceStatus.objects.get(id=RaceStatus.RACE_STATUS_DEFAULT)
         self.race.save()
-        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}))
-        self.assertEqual(response.status_code, 200)
+        response = self.client.post(reverse("regulations_setup", kwargs={"pk":self.race.id}), params)
+        self.assertEqual(response.status_code, 302)
 
