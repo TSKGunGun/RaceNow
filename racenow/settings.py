7c31223cbedfe94887f318807469758baf223b1f
TSKGunGun <stonebeach.dakar@gmail.com>
2022-03-11 10:34:49 +0900

    シークレットキーのブロック

--- racenow/settings.py
+++ racenow/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n=f769xtr+ycps!q00bje10$3x&vlh*9%^80*b&^^tvx@1t3*i'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
